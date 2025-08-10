25d6aa071748cdafdb933eb8ccffd848fa346202
Liu <18409283+ls125781003@users.noreply.github.com>
2025-08-10 22:17:26 +0800

    0810beta

--- controllers/index.js
+++ controllers/index.js
@@ -8,6 +8,8 @@ import decoderController from './decoder.js';
 import authCoderController from './authcoder.js';
 import webController from './web.js';
 import httpController from './http.js';
+// import taskController from './tasker.js';
+import cronTaskerController from './cron-tasker.js';
 
 export const registerRoutes = (fastify, options) => {
     fastify.register(docsController, options);
@@ -20,4 +22,6 @@ export const registerRoutes = (fastify, options) => {
     fastify.register(authCoderController, options);
     fastify.register(webController, options);
     fastify.register(httpController, options);
+    // fastify.register(taskController, options);
+    fastify.register(cronTaskerController, options);
 };
